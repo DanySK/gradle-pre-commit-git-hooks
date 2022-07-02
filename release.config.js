75553d494e98d571fdb9bdd4a0e6fa978d9a51ca
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-02 09:28:16 +0200

    ci: add dry-deployment test and fix the release process for publish-on-central 2.x.x

--- release.config.js
+++ release.config.js
@@ -1,8 +1,15 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
-./gradlew releaseKotlinMavenOnMavenCentralNexus || exit 1
-./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
+./gradlew \
+    uploadKotlin \
+    uploadPluginMavenToMavenCentralNexus \
+    uploadGitHooksPluginMarkerMavenToMavenCentralNexus \
+    release || exit 1
+./gradlew \
+    publishPlugins \
+    -Pgradle.publish.key=$GRADLE_PUBLISH_KEY \
+    -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
 ./gradlew publishKotlinMavenPublicationToGithubRepository || true
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
