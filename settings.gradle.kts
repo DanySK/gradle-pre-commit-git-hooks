f340182ee0ae221e54bd74456b576b9c88fb98e8
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-07 22:27:49 +0200

    build: enable this plugin on itself

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.gradle.enterprise") version "3.10.2"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.14"
 }
 
 gradleEnterprise {
@@ -10,5 +11,12 @@ gradleEnterprise {
     }
 }
 
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
+
 rootProject.name = "gradle-pre-commit-git-hooks"
-enableFeaturePreview("VERSION_CATALOGS")
