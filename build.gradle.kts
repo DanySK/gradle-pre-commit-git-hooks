051b3051c31a447b22fcaac494a2c72bb2b7f1e7
Danilo Pianini <danilo.pianini@gmail.com>
2025-08-16 20:58:26 +0200

    fix: set the same java requirement Gradle has

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,8 @@ repositories {
 }
 
 multiJvm {
-    maximumSupportedJvmVersion.set(latestJavaSupportedByGradle)
+    jvmVersionForCompilation = oldestJavaSupportedByGradle
+    maximumSupportedJvmVersion = latestJavaSupportedByGradle
 }
 
 dependencies {
